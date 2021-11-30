0a45f50622a687b51506d2e67e5b736b4861576f
Peichun <a12345615241@gmail.com>
2021-11-30 15:19:15 +0800

    3rd Version

--- githubSample/src/AppTest/app.java
+++ githubSample/src/AppTest/app.java
@@ -3,8 +3,8 @@ package AppTest;
 public class app {
 
 	public static void main(String[] args) {
-		System.out.println("2nd Version");
-
+		System.out.println(" 3rd Version ");
+		System.out.println(" 3rd Version ");
 	}
 
 }
